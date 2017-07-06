6033630c3c6ae67582773411d9390804062bb654
Rasmus Jürs <rasmus@jurs.me>
2017-07-06 15:13:39 +0200

    Setup framework

---
+++ inzite-intranet.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Inzite Intranet
+Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
+Description: Basic WordPress Plugin Header Comment
+Version:     20160911
+Author:      WordPress.org
+Author URI:  https://developer.wordpress.org/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: inz_i
+Domain Path: /languages
+*/
